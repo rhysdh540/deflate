4638af537570925b58a95cc0c9ef1a268896a4c5
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-08-15 23:38:08 -0400

    disable alignment in BitCounter saves a few percent

--- src/main/kotlin/dev/rdh/deflate/util/BitCounter.kt
+++ src/main/kotlin/dev/rdh/deflate/util/BitCounter.kt
@@ -2,42 +2,30 @@ package dev.rdh.deflate.util
 
 class BitCounter : BitSink {
     var bitsWritten = 0L; private set
-    private var pos = 0
 
     override val isAligned: Boolean
-        get() = pos == 0
+        get() {
+            throw NotImplementedError("BitCounter does not support alignment checks")
+        }
 
     override fun writeBit(bit: Boolean) {
         bitsWritten++
-        pos++
-        if (pos == 8) {
-            pos = 0
-        }
     }
 
     override fun writeBits(value: Int, n: Int) {
         require(n in 1..32) { "n must be between 1 and 32" }
         bitsWritten += n
-        pos += n
-        if (pos >= 8) {
-            pos = pos and 7
-        }
     }
 
     override fun alignToByte() {
-        if (pos > 0) {
-            bitsWritten += (8 - pos)
-            pos = 0
-        }
+        throw NotImplementedError("BitCounter does not support alignment checks")
     }
 
     override fun writeAlignedByte(b: Int) {
-        check(pos == 0) { "Not byte-aligned" }
         bitsWritten += 8
     }
 
     override fun writeAlignedLE16(v: Int) {
-        check(pos == 0) { "Not byte-aligned" }
         bitsWritten += 16
     }
 
