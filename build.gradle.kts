eaeec86602669a7a51d065f62bd4eec86aab3aae
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-08-17 23:50:18 -0400

    gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,17 @@ kotlin {
     jvmToolchain(8)
 }
 
+for (file in fileTree("src/test/kotlin/demo")) {
+    if (file.isFile && file.extension == "kt") {
+        tasks.register<JavaExec>("runDemo_${file.nameWithoutExtension}") {
+            group = "demo"
+            description = "Run demo: ${file.nameWithoutExtension}"
+            classpath = sourceSets["test"].runtimeClasspath
+            mainClass.set("demo.${file.nameWithoutExtension}Kt")
+        }
+    }
+}
+
 dependencies {
     implementation("it.unimi.dsi:fastutil:8.5.16")
     testImplementation(kotlin("test"))
